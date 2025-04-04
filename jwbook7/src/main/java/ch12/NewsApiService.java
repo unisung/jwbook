c71e08a40a4ce893803b41909c991b6abd2d4700
unisung <vctor@naver.com>
2025-04-04 15:23:34 +0900

    getNews -path variable

--- jwbook7/src/main/java/ch12/NewsApiService.java
+++ jwbook7/src/main/java/ch12/NewsApiService.java
@@ -2,10 +2,12 @@ package ch12;
 
 import java.util.List;
 
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;//ws.rs패키지
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -17,13 +19,13 @@ public class NewsApiService {
 	NewsDAO dao;
 
 	public NewsApiService() {
-		dao = new NewsDAO(); //주입
+		dao = new NewsDAO(); // 주입
 	}
-	
-	//뉴스 목록
-	@GET  // GET  localhost:8080/jwbook7/api/news
+
+	// 뉴스 목록
+	@GET // GET localhost:8080/jwbook7/api/news
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<News> getNewsList(){
+	public List<News> getNewsList() {
 		List<News> newsList = null;
 		try {
 			newsList = dao.getAll();
@@ -32,8 +34,8 @@ public class NewsApiService {
 		}
 		return newsList;
 	}
-	
-	//뉴스 등록
+
+	// 뉴스 등록
 	@POST  //  POST localhost:8080/jwbook7/api/news
 	@Consumes(MediaType.APPLICATION_JSON)//클라이언트로 부터 넘어온 데이타가 JSON타입
 	public  String addNews(News news) {
@@ -46,5 +48,19 @@ public class NewsApiService {
 		}
 		return "News API: 뉴스 등록 성공!!";
 	}
-	
+
+	// 뉴스 상세 정보
+	@GET
+	@Path("{aid}") //path variable {변수}
+	@Produces(MediaType.APPLICATION_JSON)
+	public News getNews(@PathParam("aid") int aid) {
+		System.out.println("aid:"+aid);
+		News news = null;
+		try {
+			  news = dao.getNews(aid);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return news;
+	}
 }
